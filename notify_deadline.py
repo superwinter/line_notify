6cf298ec535643467f99406e323ae16a975c5860
Travis CI <wirecard@travis-ci.org>
2020-07-14 17:25:22 +0800

    fine-tune code

--- notify_deadline.py
+++ notify_deadline.py
@@ -6,21 +6,15 @@ import datetime
 import sys
 from sys import argv
 
-today = datetime.date.today()
-deadline = datetime.date(int(argv[1]),int(argv[2]),int(argv[3]))
-result = deadline - today
-if result.days < 0:
-    message = "距離 {}-{}-{} 已經到期!!".format(argv[1], argv[2], argv[3])
-else: 
-    message = "距離 {}-{}-{} 還剩 {} 天!!".format(argv[1], argv[2], argv[3], result.days)
-print("提醒宇柏,心無旁鶩\n大學學測考試:")
-print(message)
-today = datetime.date.today()
-deadline = datetime.date(int(argv[4]),int(argv[5]),int(argv[6]))
-result = deadline - today
-if result.days < 0:
-    message = "距離 {}-{}-{} 已經到期!!".format(argv[4], argv[5], argv[6])
-else:
-    message = "距離 {}-{}-{} 還剩 {} 天!!".format(argv[4], argv[5], argv[6], result.days)
-print("\n提醒宇柏,再接再勵\n大學音樂組術科考試:")
-print(message)
+def get_message(msg, year, month, day):
+    today = datetime.date.today()
+    deadline = datetime.date(int(year),int(month),int(day))
+    result = deadline - today
+    print(msg)
+    if result.days < 0:
+        print("距離 {}-{}-{} 已經到期!!".format(year, month, day))
+    else:
+        print("距離 {}-{}-{} 還剩 {} 天!!".format(year, month, day, result.days))
+
+get_message("提醒宇柏,心無旁鶩\n大學學測考試:", argv[1], argv[2], argv[3])
+get_message("\n提醒宇柏,再接再勵\n大學音樂組術科考試:", argv[4], argv[5], argv[6])
